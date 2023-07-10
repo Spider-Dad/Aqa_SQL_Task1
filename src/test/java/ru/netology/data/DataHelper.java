a49d45310a8991c5160f225bf05387f4e41bffd1
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-07-10 17:37:29 +0300

    Update

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,23 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+
+
+    private DataHelper() {
+    }
+
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+}
